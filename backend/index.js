b4f97a5f2732905402c00cbaeabd44a3c87272bc
hardik-rl <hardik.radicalloop@gmail.com>
2025-06-18 23:37:00 +0530

    room chatapp create

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const Razorpay = require("razorpay");
 const cors = require("cors");
+const socketIO = require("socket.io");
+const http = require("http");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-const paymentRoutes = require("./routes/paymentRoutes");
 
+const paymentRoutes = require("./routes/paymentRoutes");
 app.use("/", paymentRoutes);
 
+const server = http.createServer(app);
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const io = socketIO(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("send_message", (data) => {
+    io.emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+server.listen(5000, () => console.log("Server running on http://localhost:5000"));
